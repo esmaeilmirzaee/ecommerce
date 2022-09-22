85bfb519893735ada15e6168e5020ff372436059
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-22 15:02:54 +0330

    Set up second custom admin page

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+
+class BookstoreAdmin(admin.AdminSite):
+    site_title = 'Bookstore admin page'
+
+
+bookstore_admin = BookstoreAdmin(name='Bookstore admin')
+bookstore_admin.register(Book)
--- conf/urls.py
+++ conf/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
 from blog.admin import blog_admin
+from bookstore.admin import bookstore_admin
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blogadmin/', blog_admin.urls),
+    path('bookstoreadmin/', bookstore_admin.urls),
 ]
 
 admin.site.index_title = 'BookStore'
