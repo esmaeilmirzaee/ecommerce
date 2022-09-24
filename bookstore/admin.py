0107c65d6ff263fe31bcbc42008b9ebe23e51174
Esmaeil MIRZAEE <EsmaeilMIRZAEE@gmail.com>
2022-09-24 21:13:46 +0330

    Change user model

--- bookstore/admin.py
+++ bookstore/admin.py
@@ -6,22 +6,30 @@ class BookstoreAdmin(admin.AdminSite):
     site_title = 'Bookstore admin page'
 
 
-class Filter(admin.ModelAdmin):
-    list_display = ('id', 'email', 'created_at', 'role', 'is_active',)
-    list_filter = ('is_active', 'role', 'created_at',)
-
-
 class EmailFilter(admin.SimpleListFilter):
-    title = ''
-    parameter_name = ''
+    title = 'Email Filter'
+    parameter_name = 'email'
 
     def lookups(self, request, model_admin):
-        return
+        return (
+            ('has_email', 'has_email'),
+            ('no_email', 'no_email'),
+        )
 
     def queryset(self, request, queryset):
-        return
+        if not self.value():
+            return queryset
+        if self.value().lower() == 'has_email':
+            return queryset.exclude(user__email=None)
+        if self.value().lower() == 'no_email':
+            return queryset.filter(user__email='')
+
+
+class Filter(admin.ModelAdmin):
+    
+    list_filter = ('is_active', 'role', 'created_at', EmailFilter, 'user__email')
 
 
 bookstore_admin = BookstoreAdmin(name='Bookstore admin')
-bookstore_admin.register(Book)
+# bookstore_admin.register(Book)
 bookstore_admin.register(Profile, Filter)
